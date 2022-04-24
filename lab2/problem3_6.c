027d9ada915b5110f004d57eaad0125b07638f22
Nicaela Rose <58701025+nicaelarose@users.noreply.github.com>
2022-04-24 14:03:03 -0500

    Add files via upload

---
+++ lab2/problem3_6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+int main() {
+	int i=0, child;
+	child = fork();
+	if (child == 0) {
+		while(1) {
+			i++;
+			printf("Child at count %d\n", i);
+			/*sleep for 1/100 of a second*/
+			usleep(10000);
+		}
+	} else {
+		printf("Parent sleeping\n");
+		sleep(10);
+		kill(child, SIGTERM);
+		printf("Child has been killed. Waiting for it...\n");
+		wait(NULL);
+		printf("done.\n");
+	}
+	return 0;
+}
